a2964a1c3d38d7983cedea6dd5eb148aad63b49a
za-lizhiqin <lizhiqin@zhongan.io>
2019-01-18 18:40:43 +0800

    add menus

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,49 @@
-import {
-  JupyterLab, JupyterLabPlugin
-} from '@jupyterlab/application';
+import {JupyterLab, JupyterLabPlugin} from '@jupyterlab/application';
 
 import '../style/index.css';
+
+import {IMainMenu} from '@jupyterlab/mainmenu';
+
+import {Menu} from '@phosphor/widgets';
+
+import {ICommandPalette} from '@jupyterlab/apputils';
 
 
 /**
  * Initialization data for the jupyterlab-countdown extension.
  */
 const extension: JupyterLabPlugin<void> = {
-  id: 'jupyterlab-countdown',
-  autoStart: true,
-  activate: (app: JupyterLab) => {
-    console.log('hello JupyterLab!');
-    console.log('JupyterLab extension jupyterlab-countdown is activated!');
-    setInterval(() => {
-        console.log('time:',new Date())
-      }, 1000)
-  }
+    id: 'jupyterlab-countdown',
+    autoStart: true,
+    requires: [ICommandPalette, IMainMenu],
+    activate: (
+        app: JupyterLab,
+        palette: ICommandPalette,
+        mainMenu: IMainMenu
+    ) => {
+        console.log('hello JupyterLab!');
+        console.log('JupyterLab extension jupyterlab-countdown is activated!');
+        // setInterval(() => {
+        //     console.log('time:',new Date())
+        //   }, 1000)
+        const {commands} = app;
+        let command = 'labtutorial';
+        let category = 'Tutorial';
+        commands.addCommand(command, {
+            label: 'New Labtutorial',
+            caption: 'Open the Labtutorial',
+            execute: (args) => {
+                console.log('Hey')
+            }
+        });
+        palette.addItem({command, category});
+
+        let tutorialMenu: Menu = new Menu({commands});
+
+        tutorialMenu.title.label = 'Tutorial';
+        mainMenu.addMenu(tutorialMenu, {rank: 80});
+        tutorialMenu.addItem({command});
+    }
 };
 
 export default extension;
